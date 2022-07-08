460f5fc8287827c2684fdb54392b6a534653a8b7
EssLemint <limemint1396@gmail.com>
2022-07-08 21:58:49 +0900

    22-07-07

--- study-aws-springboot/src/main/java/com/lemint/book/springboot/service/posts/PostsService.java
+++ study-aws-springboot/src/main/java/com/lemint/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.lemint.book.springboot.service.posts;
 
 import com.lemint.book.springboot.domain.posts.Posts;
 import com.lemint.book.springboot.domain.posts.PostsRepository;
+import com.lemint.book.springboot.web.dto.PostsListResponseDto;
 import com.lemint.book.springboot.web.dto.PostsResponseDto;
 import com.lemint.book.springboot.web.dto.PostsSaveRequestDto;
 import com.lemint.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,10 +33,24 @@ public class PostsService {
     return id;
   }
 
+  @Transactional
+  public void delete(Long id) {
+    Posts posts = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+    postsRepository.delete(posts);
+  }
+
   public PostsResponseDto findById(Long id){
     Posts entity = postsRepository.findById(id)
         .orElseThrow(() -> new IllegalArgumentException("게시글 없음 ,id =" + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+        .map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
   }
 }
